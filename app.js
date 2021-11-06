1ebf3f6f5afacdda2f63753e42a318a267fd4ec9
Liam O'Shea <loshea@rolling-wave.com>
2021-11-06 19:59:56 -0300

    Reconfigure

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
 
+app.use(express.static("public"))
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
@@ -9,3 +10,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 app.get('/express_backend', (req, res) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
+
+
